cbdf2fb170d2c690aeb83b9523041cf93a43325a
Przemek <p.wojcinowicz@gmail.com>
2019-03-28 20:38:45 +0100

    loading classes added

--- programowanie w języku java/zad2/src/zad2/Main.java
+++ programowanie w języku java/zad2/src/zad2/Main.java
@@ -1,8 +1,10 @@
 package zad2;
 
+import java.net.MalformedURLException;
+
 public class Main
 {
-	public static void main(String[] args) throws ClassNotFoundException
+	public static void main(String[] args) throws ClassNotFoundException, MalformedURLException
 	{
 		MainMenu.menu();
 	}
--- programowanie w języku java/zad2/src/zad2/MainMenu.java
+++ programowanie w języku java/zad2/src/zad2/MainMenu.java
@@ -1,6 +1,7 @@
 package zad2;
 
 import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.LinkedList;
@@ -12,50 +13,52 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-public class MainMenu 
-{
-	public static void menu() throws ClassNotFoundException
-	{
-		int choose;
-		File file = new File("D:\\eclipse-workspace\\zad2\\algorytmy");
-		URL[] urlsToLoadForm = new URL[] {};
-		URLClassLoader loader = new URLClassLoader(urlsToLoadForm);
+public class MainMenu {
+	public static void menu() throws ClassNotFoundException, MalformedURLException {
+
+		File file = new File("");
+		URL[] urlsToLoadFrom = new URL[] { file.toURI().toURL() };
+		URLClassLoader loader = new URLClassLoader(urlsToLoadFrom);
 		LinkedList<Class> classes = new LinkedList<Class>();
 		System.out.println("1. Zaladuj klasy");
 		System.out.println("2. Rozwiaz problem plecakowy");
 		System.out.println("3. Wyladuj klasy");
 		System.out.println("0. Zakoncz");
 		Scanner scan = new Scanner(System.in);
-		choose = scan.nextInt();
-	    while(choose !=0)
-	    {
-	    	switch(choose)
-	    	{
-	    	case 1:
-	    		classes.add(Class.forName("Brute_force", false, loader));
-	    		classes.add(Class.forName("Greedy", false, loader));
-	    		break;
-	    	case 2:
-	    	//	try
-	    		//{
-	    			random();
-			//	} catch (Exception e) {
-			//		System.out.println("brak zaladownych klas");
-			//	}
-	    		break;
-	    	case 3:
-	    		classes.remove(0);
-	    		classes.remove(1);
-	    		
-	    	default:
-	    		System.out.println("Wybierz dostepna opjce");
-	    		choose = scan.nextInt();
-	    	}
-	    }
-		
+		int off = 1;
+		int choose;
+		while (off != 0) {
+			choose = scan.nextInt();
+			switch (choose) {
+			case 1:
+				classes.add(Class.forName("zad2.Brute_force", false, loader));
+				classes.add(Class.forName("zad2.Greedy", false, loader));
+				System.out.println("Klasy zaladowane");
+				break;
+			case 2:
+				// try
+				// {
+				random();
+				// } catch (Exception e) {
+				// System.out.println("brak zaladownych klas");
+				// }
+				break;
+			case 3:
+				classes.remove(0);
+				classes.remove(1);
+				break;
+			case 0:
+				off = 0;
+				break;
+
+			default:
+				System.out.println("Wybierz dostepna opjce");
+				choose = scan.nextInt();
+			}
+		}
+
 	}
-	
-	
+
 	public static void random() {
 		Random r = new Random();
 		long bean = r.nextInt(18);
@@ -64,18 +67,18 @@ public class MainMenu
 		Map<Long, List<Items>> map = new HashMap<Long, List<Items>>();
 		SoftReference reference = new SoftReference(map, reference_queue);
 		Threads[] threads = new Threads[5];
-		//Threads threads;
+		// Threads threads;
 
 		while (true) {
 			for (int i = 0; i < 2; i++) {
 				random_bean = r.nextInt(18) + 1;
 				threads[i] = new Threads(map, random_bean, reference, i);
 				threads[i].start();
-		//System.out.println("Podaj rb: ");
-		//		Scanner sc = new Scanner(System.in);
-		//		long rb = sc.nextInt();
-		//		threads = new Threads(map, rb, reference, 1);
-		//		threads.start();
+				// System.out.println("Podaj rb: ");
+				// Scanner sc = new Scanner(System.in);
+				// long rb = sc.nextInt();
+				// threads = new Threads(map, rb, reference, 1);
+				// threads.start();
 			}
 		}
 
